73ddced5c14710ebcc1ab16dea587097ebf3bb3d
VoronKor <ravorr@yandex.ru>
2020-10-31 22:03:18 +0300

    branch commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,8 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("5 раз");
         System.out.println("second commit");
+
+        System.out.println("master");
     }
 }
 
