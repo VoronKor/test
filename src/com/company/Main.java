f71e8c7fd00008998dc8a26434b384a6392b5723
VoronKor <ravorr@yandex.ru>
2020-10-31 21:34:13 +0300

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // просто запомнить
+
+        System.out.println("Hello world");
+        System.out.println("5 раз");
+    }
+}
+
